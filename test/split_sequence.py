ac7eb097d9be21496f6bdeeea41243570d6c51f9
950288 <950288s@gmail.com>
2023-07-22 20:34:45 +0800

    test: create split_sequence.py

---
+++ test/split_sequence.py
@@ -0,0 +1,33 @@
+import numpy as np
+from collections import defaultdict
+import pickle
+import torch.nn as nn
+
+seq = 'MVSKGEELFTGVVPILVELDGDVNGHKFSVSGEGEGDATYGKLTLKFICTTGKLPVPWPTLVTTFSYGVQCFSRYPDHMKQHDFFKSAMPEGYVQERTIFFKDDGNYKTRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITLGMDELYK'
+
+# seq2 =  'VQERTIFFKDGNYKMVSKEELFTGVPILVELDGDVNHKFSVSGEGEGDAYGKLTLKFIACTTGKLPVPWPTLVTTFSYGVQCFSRYPDHMKQHDFFKSAMPETRAEVKFEGDTLVNRIELKGIDFKEDGNILGHKLEYNYNSHNVYIMADKQKNGIKVNFKIRHNIEDGSVQLADHYQQNTPIGDGPVLLPDNHYLSTQSALSKDPNEKRDHMVLLEFVTAAGITLGMDELYK'
+
+word_dict = defaultdict(lambda: len(word_dict))
+
+def dump_dictionary(dictionary, filename):
+    with open(filename, 'wb') as file:
+        pickle.dump(dict(dictionary), file)
+
+def split_sequence(sequence, ngram):
+    sequence = '-' + sequence + '='
+    # print(sequence)
+    words = [word_dict[sequence[i:i+ngram]] for i in range(len(sequence)-ngram+1)]
+    return np.array(words)
+
+word = split_sequence(seq, 3)
+
+embed_word = nn.Embedding(len(word_dict), 3)
+
+word_vectors = embed_word(word)
+
+print(word_vectors)
+
+# print(split_sequence(seq2, 3))
+# print(word_dict)
+
+# dump_dictionary(word_dict, './sequence_dict.pickle')
